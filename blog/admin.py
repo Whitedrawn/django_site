cf43a92416a6881dc7d368b542fd2c9f74582316
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-12-19 18:15:40 +0000

    adding models for the database

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author,Tag,Post
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter=("author","tags","date",)
+    list_display=("title","date","author",)
+    prepopulated_fields={"slug":("title",)}
+
+
+admin.site.register(Author)
+
+admin.site.register(Tag)
+
+admin.site.register(Post,PostAdmin)
