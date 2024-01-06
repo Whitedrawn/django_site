4592a0c6c3046a32b1d1f2063e94d3210e459f02
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2024-01-06 16:36:08 +0000

    adding image upload and switching to class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
- path("",views.starting_page,name="starting-page"),   
- path("posts/",views.posts,name="posts-page"),   
- path("posts/<slug:slug>",views.post_detail,name="post-detail-page")   
-]
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts/", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_site/settings.py
+++ django_site/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
+MEDIA_ROOT=BASE_DIR/"uploads"
+MEDIA_URL="/files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
