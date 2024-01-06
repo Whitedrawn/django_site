e6edbb89f9e7d515b7f3ccba63d36cf26c03a416
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2024-01-06 23:23:13 +0000

    added comments feature

--- blog/admin.py
+++ blog/admin.py
@@ -2,22 +2,29 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author,Tag,Post
+from .models import Author, Tag, Post, Comment
+
 
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
+
 class TagAdmin(admin.ModelAdmin):
     pass
 
-class PostAdmin(admin.ModelAdmin):
-    list_filter=("author","tags","date",)
-    list_display=("title","date","author",)
-    prepopulated_fields={"slug":("title",)}
 
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=("user_name","post")
 
 admin.site.register(Author)
 
 admin.site.register(Tag)
 
-admin.site.register(Post,PostAdmin)
+admin.site.register(Post, PostAdmin)
+
+admin.site.register(Comment,CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image=models.ImageField(upload_to="posts",null=True)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
@@ -34,3 +34,14 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag)
     author = models.ForeignKey(
         Author, related_name="posts", null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField(max_length=254)
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
