59cd4aebfdc21d071337391c694f6888acc8cdfb
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2024-01-07 00:31:09 +0000

    added read later list feature

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts/", views.AllPostsView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
+    path("read-later",views.ReadLaterView.as_view(),name="read-later")
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
