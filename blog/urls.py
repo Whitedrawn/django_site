b416f1f5e25ae5f54341f236eb49e328e49c3954
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2024-01-08 12:03:47 +0000

    last commit before deployment

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from . import views
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts/", views.AllPostsView.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
     path("read-later",views.ReadLaterView.as_view(),name="read-later")
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
